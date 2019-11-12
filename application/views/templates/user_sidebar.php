35bfd5c522f09e8d3eeeed433fbd8fb6da120a95
julians22 <dabnerjulian@yahoo.com>
2019-11-12 18:11:14 +0700

    edit

--- application/views/templates/user_sidebar.php
+++ application/views/templates/user_sidebar.php
@@ -59,7 +59,7 @@
 
         <?php endforeach; ?>
         <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('auth/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
+            <a class="nav-link" href="<?= base_url('auth/logout'); ?>" id="logoutBtn">
                 <i class="fas fa-fw fa-sign-out-alt"></i>
                 <span>Logout</span></a>
         </li>
--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -29,4 +29,24 @@ $('.hapus').on('click', function (e) {
 				document.location.href = href;
 			}
 		});
-})
+});
+
+
+
+$('.logoutBtn').on('click', function (e) {
+    e.preventDefault();
+    const href = $(this).attr('href');
+    Swal.fire({
+			title: "Are you sure?",
+			text: "This data will deleted!",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete it!"
+		}).then(result => {
+			if (result.value) {
+				document.location.href = href;
+			}
+		});
+});
